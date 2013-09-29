f5555ac2cbc8059e0df4cad6fe3c6a22f646b176
Matias Fuentes <thelegendrymatias@hotmail.com>
2013-09-29 16:14:55 -0300

    Se agrega implementacion de metodos en class Mano y Unit Test

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -1,27 +1,40 @@
 package edu.tallerweb.pptls;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Representa una de las Manos involucradas en el juego
  */
 public class Mano {
-
-	/**
-	 * Toda Mano debe crearse con una forma dada, que será
-	 * la que determine su condición en el juego.
-	 * @param forma, la Forma que adopta la Mano.
-	 */
+	Forma forma;
+	
+	private static final Map<Integer, List<Integer>> juego = new HashMap<Integer,List<Integer>>(){
+		{
+			put(0, Arrays.asList(3,4));
+			put(1, Arrays.asList(0,4));
+			put(2, Arrays.asList(0,1));
+			put(3, Arrays.asList(1,2));
+			put(4, Arrays.asList(2,3));
+		}
+	};
+	
 	public Mano(final Forma forma) {
-		throw new RuntimeException("No implementado aún");
+		this.forma = forma;
 	}
 
-	/**
-	 * Evaluará el resultado de la partida según las reglas
-	 * del juego.
-	 * @param otra, la otra Mano.
-	 * @return un Resultado, de acuerdo al estado del juego.
-	 */
 	public Resultado jugarCon(final Mano otra) {
-		throw new RuntimeException("No implementado aún");
+		if (juego.get(this.forma.getValor()).contains(otra.forma.getValor())){
+			return Resultado.GANA;
+		}
+		else if (this.forma.getValor() == otra.forma.getValor()){
+			return Resultado.EMPATA;
+		}
+		
+		return Resultado.PIERDE;
 	}
 
 }
--- src/test/java/edu/tallerweb/pptls/PartidaTests.java
+++ src/test/java/edu/tallerweb/pptls/PartidaTests.java
@@ -16,4 +16,103 @@ public class PartidaTests {
 
 	}
 
+	@Test
+	public void queSpockDestrozaTijera() {
+
+		Mano jugadorUno = new Mano(Forma.SPOCK);
+		Mano jugadorDos = new Mano(Forma.TIJERA);
+
+		assertEquals("Spock destroza tijera",
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+
+	}
+
+	@Test
+	public void queTijeraCortaPapel() {
+
+		Mano jugadorUno = new Mano(Forma.TIJERA);
+		Mano jugadorDos = new Mano(Forma.PAPEL);
+
+		assertEquals("Tijera corta papel",
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+
+	}
+
+	@Test
+	public void queTijeraDecapitaLagarto() {
+
+		Mano jugadorUno = new Mano(Forma.TIJERA);
+		Mano jugadorDos = new Mano(Forma.LAGARTO);
+
+		assertEquals("Tijera decapita lagarto",
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+
+	}
+
+	@Test
+	public void quePapelCubrePiedra() {
+
+		Mano jugadorUno = new Mano(Forma.PAPEL);
+		Mano jugadorDos = new Mano(Forma.PIEDRA);
+
+		assertEquals("Papel cubre piedra",
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+
+	}
+
+	@Test
+	public void quePapelRefutaSpock() {
+
+		Mano jugadorUno = new Mano(Forma.PAPEL);
+		Mano jugadorDos = new Mano(Forma.SPOCK);
+
+		assertEquals("Papel refuta spock",
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+
+	}
+
+	@Test
+	public void quePiedraAplastaLagarto() {
+
+		Mano jugadorUno = new Mano(Forma.PIEDRA);
+		Mano jugadorDos = new Mano(Forma.LAGARTO);
+
+		assertEquals("Piedra aplasta lagarto",
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+
+	}
+
+	@Test
+	public void quePiedraAplastaTijera() {
+
+		Mano jugadorUno = new Mano(Forma.PIEDRA);
+		Mano jugadorDos = new Mano(Forma.TIJERA);
+
+		assertEquals("Piedra aplasta tijera",
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+
+	}
+
+	@Test
+	public void queLagartoEnvenenaSpock() {
+
+		Mano jugadorUno = new Mano(Forma.LAGARTO);
+		Mano jugadorDos = new Mano(Forma.SPOCK);
+
+		assertEquals("Lagarto envenera spock",
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+
+	}
+
+	@Test
+	public void queLagartoComePapel() {
+
+		Mano jugadorUno = new Mano(Forma.LAGARTO);
+		Mano jugadorDos = new Mano(Forma.PAPEL);
+
+		assertEquals("Lagarto come papel",
+				Resultado.GANA, jugadorUno.jugarCon(jugadorDos));
+
+	}
+
 }
